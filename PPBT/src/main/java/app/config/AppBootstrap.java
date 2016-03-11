356a194c6e3c60c6a4d8fa8d62543c39633b5d30
Shitianyu Pan <pan.210@buckeyemail.osu.edu>
2016-03-10 21:20:48 -0600

    add config

---
+++ PPBT/src/main/java/app/config/AppBootstrap.java
@@ -0,0 +1,34 @@
+package app.config;
+
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import com.sfs.services.ServiceModule;
+import org.javalite.activeweb.AppContext;
+import org.javalite.activeweb.Bootstrap;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Created by Doublefinger on 1/4/16.
+ */
+public class AppBootstrap extends Bootstrap {
+
+    @Override
+    public void init(AppContext context) {
+        context.set("spring", new ClassPathXmlApplicationContext("spring.xml"));
+    }
+
+    protected Injector getInjector() {
+        return Guice.createInjector(new ServiceModule());
+    }
+
+    @Override
+    public void destroy(AppContext context) {
+        ApplicationContext spring = (ApplicationContext) context.get("spring");
+        if (spring instanceof ConfigurableApplicationContext) {
+            ((ConfigurableApplicationContext) spring).close();
+        }
+        super.destroy(context);
+    }
+}
---
+++ PPBT/src/main/java/app/config/AppControllerConfig.java
@@ -0,0 +1,15 @@
+package app.config;
+
+import org.javalite.activeweb.AbstractControllerConfig;
+import org.javalite.activeweb.AppContext;
+
+/**
+ * Created by Doublefinger on 1/4/16.
+ *
+ */
+public class AppControllerConfig extends AbstractControllerConfig {
+    @Override
+    public void init(AppContext context) {
+
+    }
+}
