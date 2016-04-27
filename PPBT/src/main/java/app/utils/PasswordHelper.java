b1696ee65fb031d0c2da467d87cf1b4ea1fd1906
S, Pan <pan.210@buckeyemail.osu.edu>
2016-04-27 02:47:36 -0500

    Update PasswordHelper.java

--- PPBT/src/main/java/app/utils/PasswordHelper.java
+++ PPBT/src/main/java/app/utils/PasswordHelper.java
@@ -20,14 +20,6 @@ public class PasswordHelper {
         return Base64.encodeBase64String(buffer);
     }
 
-    public static String sHA1(String message){
-        return String.valueOf(DigestUtils.sha1(message));
-    }
-
-    public static String uTF8SHA1(String message){
-        return String.valueOf(DigestUtils.sha1(message.getBytes(Charset.forName("UTF-8"))));
-    }
-
     // Encode password
     public static String encodePassword(String user, String pass, String salt){
         byte[] bUser = user.getBytes(Charset.forName("UTF-16LE"));
@@ -41,26 +33,5 @@ public class PasswordHelper {
         System.arraycopy(bPass, 0, bAll, bUser.length + bSalt.length, bPass.length);
         bRet = DigestUtils.sha1(bAll);
         return Base64.encodeBase64String(bRet);
-    }
-
-    private static final String passwordChars = "34678abdefgjmprtwy";
-    private static Random random = new Random();
-
-    public static String randomPassword(int length){
-        String password = "";
-        for (int i = 0; i < length; i++){
-            password += passwordChars.charAt(random.nextInt(passwordChars.length()));
-        }
-        return password;
-    }
-
-    public static final String Alphabet = "abcdefghijklmnopqrstuvwyxzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-
-    public static String randomAlphabetString(int length){
-        String password = "";
-        for (int i = 0; i < length; i++){
-            password += Alphabet.charAt(random.nextInt(Alphabet.length()));
-        }
-        return password;
     }
 }
